86fd8bf6434d7869c1f4349bd301c1ed32784a4f
gerry.bradley <gerry bradley>
2017-09-07 08:59:41 -0700

    adding stuff

--- src/main/java/com/liberymutual/goforcode/recipe/controllers/RecipeApiController.java
+++ src/main/java/com/liberymutual/goforcode/recipe/controllers/RecipeApiController.java
@@ -22,47 +22,53 @@ import com.liberymutual.goforcode.recipe.repositories.RecipeRepository;
 @RequestMapping("/recipes")
 
 public class RecipeApiController {
-    private RecipeRepository rcpRepo;
+    private RecipeRepository recipeRepo;
     private IngredientRepository   ingrRepo;
     private InstructionRepository   instrRepo;
     
     
     public RecipeApiController(RecipeRepository recipeRepo, IngredientRepository ingredientRepo, InstructionRepository instrRepo) {
-        this.rcpRepo  = recipeRepo;
+        this.recipeRepo  = recipeRepo;
         this.instrRepo = instrRepo;
         this.ingrRepo = ingredientRepo;
         
         List<Recipe> recipes = Arrays.asList(new Recipe[]{
-                new Recipe("Food_1", "Description1",  10),
-                new Recipe("Food_2", "Description2",  60),
-                new Recipe("Food_3", "Description3",  120)
+                new Recipe("foodOne", "descriptionOne",  10),
+                new Recipe("foodTwo", "descriptionTwo",  60),
+                new Recipe("foodThree", "descriptionThree",  120)
         });
         
-        rcpRepo.save(recipes);  
+        recipeRepo.save(recipes);  
         
-        ingrRepo.save(new Ingredient(recipes.get(0), "Meat",   "ounce", 10));  
+        ingrRepo.save(new Ingredient(recipes.get(0), "Meat",   "ounce", 10));   
         ingrRepo.save(new Ingredient(recipes.get(0), "Salt",   "ounce", 10)); 
-        ingrRepo.save(new Ingredient(recipes.get(0), "Milk",   "ounce", 10)); 
+        ingrRepo.save(new Ingredient(recipes.get(0), "Gravy",   "ounce", 10)); 
+        ingrRepo.save(new Ingredient(recipes.get(0), "Herbs",   "ounce", 10)); 
+        ingrRepo.save(new Ingredient(recipes.get(0), "Butter",   "ounce", 10)); 
+        ingrRepo.save(new Ingredient(recipes.get(0), "Seasonings",   "ounce", 10)); 
         ingrRepo.save(new Ingredient(recipes.get(1), "Potatoes", "kilo",  2)); 
         ingrRepo.save(new Ingredient(recipes.get(1), "garlic", "1/3 teaspoon", 2));
         ingrRepo.save(new Ingredient(recipes.get(1), "Cheese", "kilo",  2));
         ingrRepo.save(new Ingredient(recipes.get(1), "Ham", "kilo",  2));
         ingrRepo.save(new Ingredient(recipes.get(1), "Onions", "kilo",  2));
         ingrRepo.save(new Ingredient(recipes.get(2), "Butter",     "pound", 1));
+        ingrRepo.save(new Ingredient(recipes.get(2), "Eggs",     "pound", 1));
+        ingrRepo.save(new Ingredient(recipes.get(2), "Flour",     "pound", 1));
+        ingrRepo.save(new Ingredient(recipes.get(2), "Noodles",     "pound", 1));
       
-        instrRepo.save(new Instruction(recipes.get(0), "description1"));       
-        instrRepo.save(new Instruction(recipes.get(1), "descriptions2"));      
-        instrRepo.save(new Instruction(recipes.get(2), "descriptions3"));
+        instrRepo.save(new Instruction(recipes.get(0), "descriptionOne"));       
+        instrRepo.save(new Instruction(recipes.get(1), "descriptionTwo"));      
+        instrRepo.save(new Instruction(recipes.get(2), "descriptionThree"));
     }
     
     @GetMapping("")
     public List<Recipe> getAll() {
-        return rcpRepo.findAll();
+        return recipeRepo.findAll();
     }
     
     @GetMapping("{id}")
     public Recipe getOne(@PathVariable long id) throws RecipeNotFoundException {
-        Recipe recipe = rcpRepo.findOne(id);
+        Recipe recipe = recipeRepo.findOne(id);
         if (recipe == null) {
             throw new RecipeNotFoundException();
         }
@@ -70,24 +76,24 @@ public class RecipeApiController {
     }
       
     @PostMapping("")
-    public Recipe create(@RequestBody Recipe rcp) {
-        return rcpRepo.save(rcp);   
+    public Recipe create(@RequestBody Recipe recipe) {
+        return recipeRepo.save(recipe);   
     }   
     
     
     @PutMapping("{id}")
-    public Recipe update(@RequestBody Recipe rcp, @PathVariable long id) {
-        rcp.setId(id);
-        return rcpRepo.save(rcp);   
+    public Recipe update(@RequestBody Recipe recipe , @PathVariable long id) {
+        recipe.setId(id);
+        return recipeRepo.save(recipe);   
     }   
     
         
     @DeleteMapping("{id}")
     public Recipe delete(@PathVariable long id) {
         try {
-             Recipe rcp = rcpRepo.findOne(id);
-             rcpRepo.delete(id);
-             return rcp;            
+             Recipe recipe = recipeRepo.findOne(id);
+             recipeRepo.delete(id);
+             return recipe;            
         } catch (EmptyResultDataAccessException e) {
              return null;
         }
